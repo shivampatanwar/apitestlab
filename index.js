5f0c07bf045bef1d0cd3d241792d41e7b1b8d777
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-09-09 17:56:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+let method = document.getElementById('method');
+let url = document.getElementById('url');
+let body = document.getElementById('body');
+let btn = document.getElementById('btn');
+let response = document.getElementById('response');
+let beautify = document.getElementById('beautify');
+
+
+
+beautify.addEventListener('click', function (e) {
+    if (body.value != '') {
+        body.value = JSON.stringify(JSON.parse(body.value), null, 2);
+    }
+});
+
+
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    let header= {
+        method: method.value,
+    }   
+
+
+
+    if(method.value == 'POST' || method.value == 'PUT' || method.value=='PATCH' ){
+
+        let obj = JSON.parse(body.value);
+        header.body=JSON.stringify(obj); 
+        header.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    }
+
+    fetch(url.value, header)
+    .then(resp => resp.json())
+    .then((data) => {
+        console.log(data);
+        response.value = JSON.stringify(data, null, 2);
+
+    });
+
+});
+
+
